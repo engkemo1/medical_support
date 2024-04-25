f72976d948634dc17911bc886badbf88f0c2d921
mohamed <mezoattalla123@gmail.com>
2024-04-25 09:53:52 +0200

    Add splash screen feature with logo, gradient background, and navigation to either login or onboarding view based on user data.

---
+++ lib/features/splash/presentation/view/widgets/splash_view_body.dart
@@ -0,0 +1,38 @@
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medical_support/constants.dart';
+import 'package:medical_support/core/database/cache/cache_helper.dart';
+import 'package:medical_support/core/utils/assets.dart';
+import 'package:medical_support/core/utils/styles.dart';
+import 'package:medical_support/features/logIn/presentation/views/log_in_view.dart';
+import 'package:medical_support/features/onBoarding/presentation/view/on_boarding_view.dart';
+
+class SplashPage extends StatelessWidget {
+  SplashPage({Key? key}) : super(key: key);
+
+  final bool onBoarding = CacheHelper().getData(key: "onBoarding") ?? false;
+
+  @override
+  Widget build(BuildContext context) {
+    return EasySplashScreen(
+      logoWidth: 100.w,
+      gradientBackground: const LinearGradient(
+          colors: [Color(0xffAC7EBC), Color(0xff436973)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight),
+      title: Text(
+        "MEDICAL SUPPORT",
+        style: Styles.textStyle28.copyWith(color: kSecondaryColor),
+      ),
+      logo: Image.asset(
+        Assets.logo,
+        fit: BoxFit.cover,
+        width: 130.w,
+      ),
+      showLoader: false,
+      navigator: onBoarding ? const LogInView() : const OnBoardingView(),
+      durationInSeconds: 3,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'core/database/cache/cache_helper.dart';
+import 'features/splash/presentation/view/splash_view.dart';
+
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper().init();
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-
+    return const ScreenUtilInit(
+      designSize: Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashView(),
+      ),
     );
   }
 }
-

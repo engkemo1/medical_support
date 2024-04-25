ab956e164fd4bad03687f754005fe55d9f820490
mohamed <mezoattalla123@gmail.com>
2024-04-25 09:52:54 +0200

    Added styles, assets, and app router classes with initial implementation.

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:medical_support/features/home/presentation/views/home_view.dart';
+import 'package:medical_support/features/logIn/presentation/views/log_in_view.dart';
+import 'package:medical_support/features/onBoarding/presentation/view/on_boarding_view.dart';
+import 'package:medical_support/features/signUp/presentation/views/sign-up_view.dart';
+import 'package:medical_support/features/splash/presentation/view/splash_view.dart';
+
+abstract class AppRouter {
+  static const kSplash = '/';
+  static const kOnBoarding = '/onBoarding';
+  static const kLogInView = '/login';
+  static const kSignUpView = '/signup';
+  static const kHomeView = '/home';
+
+
+  static Route<Object?> router(RouteSettings settings) {
+    switch (settings.name) {
+      case kSplash:
+        return MaterialPageRoute(builder: (context) {
+          return const SplashView(); //replace this Scaffold with a Splash Screen
+        });
+      case kOnBoarding:
+        return MaterialPageRoute(builder: (context) {
+          return const OnBoardingView(); //replace this Scaffold with a on boarding Screen
+        });
+
+      case kLogInView:
+        return MaterialPageRoute(builder: (context) {
+          return const LogInView(); //replace this Scaffold with a login Screen
+        });
+
+
+      case kSignUpView:
+        return MaterialPageRoute(builder: (context) {
+          return  const SignUpView(); //replace this Scaffold with a signup Screen
+        });
+
+      case kHomeView:
+        return MaterialPageRoute(builder: (context) {
+          return const HomeView(); //replace this Scaffold with a Home Screen
+        });
+
+
+      default:
+        return MaterialPageRoute(builder: (context) {
+          return const Scaffold(
+            body: Center(
+              child: Text("AppStrings.notFound"),
+            ),
+          );
+        });
+    }
+  }
+}

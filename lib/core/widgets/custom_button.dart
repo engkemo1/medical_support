2951574a7ee0924f8d1248162b72b8772e5f6ceb
mohamed <mezoattalla123@gmail.com>
2024-04-25 09:57:40 +0200

    Commit message: "Add CustomButton widget with text and onPressed functionality"

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../constants.dart';
+import '../utils/styles.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  final String text;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      height: 62,
+      minWidth: MediaQuery.of(context).size.width,
+      color: kPrimaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50.sp),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: Styles.textStyle18.copyWith(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}

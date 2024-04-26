e0f4cb21cf6f26c28abae900de40c7a05d383c4c
mohamed <mezoattalla123@gmail.com>
2024-04-26 17:26:02 +0300

    Created a new custom text field widget for user input with validation and styling options.

---
+++ lib/core/widgets/custom_text_field.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:medical_support/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+    required this.prefixIcon,
+    this.onChanged,
+    this.onSaved,
+    required this.keyboardType,
+    this.obscureText = false, required this.labelText,
+  });
+
+  final Widget prefixIcon;
+  final String labelText;
+  final TextInputType keyboardType;
+  final bool obscureText;
+
+
+  final void Function(String)? onChanged;
+  final void Function(String?)? onSaved;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      cursorColor: kPrimaryColor,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "Field is required";
+        } else {
+          return null;
+        }
+      },
+
+      onSaved: onSaved,
+      onChanged: onChanged,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        prefixIcon: prefixIcon,
+        label:  Text(labelText),
+        suffixIconColor: kGreyColor,
+        enabledBorder:  OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50.0),
+          borderSide: const BorderSide(
+          ),
+        ),
+        focusedBorder:
+        OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50.0),
+          borderSide: const BorderSide(
+            color: kPrimaryColor,
+          ),
+        ),
+        errorBorder:  OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50.0),
+          borderSide: const BorderSide(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      keyboardType: keyboardType,
+    );
+  }
+}

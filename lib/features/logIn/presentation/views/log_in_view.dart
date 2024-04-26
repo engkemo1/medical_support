fe4b898000cbbcb1131892735266e10e80344f6b
mohamed <mezoattalla123@gmail.com>
2024-04-26 17:30:13 +0300

    Added new log in view and log in view body to the project with necessary imports and functionality.

---
+++ lib/features/logIn/presentation/views/log_in_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'widgets/log_in_view_body.dart';
+
+class LogInView extends StatelessWidget {
+  const LogInView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        body: LogInViewBody(),
+      ),
+    );
+  }
+}
---
+++ lib/features/logIn/presentation/views/widgets/log_in_view_body.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medical_support/constants.dart';
+import 'package:medical_support/core/utils/styles.dart';
+import 'package:medical_support/features/logIn/presentation/views/widgets/log_in_section.dart';
+import 'package:medical_support/features/logIn/presentation/views/widgets/sign_up_section.dart';
+
+class LogInViewBody extends StatefulWidget {
+  const LogInViewBody({super.key});
+
+  @override
+  State<LogInViewBody> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<LogInViewBody>
+    with SingleTickerProviderStateMixin {
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool isLoading = true;
+  late TabController tabController;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    phoneController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          child: Form(
+            key: _formKey,
+            child: SizedBox(
+              width: double.infinity,
+              height: 700.h,
+              child: Stack(
+                children: [
+                  Container(
+                    color: kPrimaryColor,
+                    width: double.infinity,
+                    height: 335.h,
+                  ),
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 35.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "مرحبا",
+                          style: Styles.textStyle35,
+                        ),
+                        Text(
+                          "من فضلك قم بتسجيل الدخول او قم بانشاء حساب جديد",
+                          style: Styles.textStyle20,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 220.0.h,
+                      right: 8.w,
+                      left: 8.w,
+                    ),
+                    child: SizedBox(
+                      height: 470,
+                      child: Card(
+                        shadowColor: Colors.black,
+                        elevation: 5.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 20),
+                            TabBar(
+                              controller: tabController,
+                              tabs: const [
+                                Tab(
+                                  text: 'حساب جديد',
+                                ),
+                                Tab(
+                                  text: 'تسجيل الدخول',
+                                ),
+                              ],
+                              labelStyle: Styles.textStyle24,
+                              labelColor: Colors.black,
+                              indicatorColor: kPrimaryColor,
+                            ),
+                            Expanded(
+                              child: TabBarView(
+                                controller: tabController,
+                                children: const [
+                                  SignUpSection(),
+                                  LogInSection(),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 530.0.h),
+                    child: Center(
+                      child: FloatingActionButton(
+                        backgroundColor: Colors.white,
+                        shape: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0.sp),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            color: Colors.transparent,
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Icon(
+                          Icons.arrow_back,
+                          color: kPrimaryColor,
+                          size: 35,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future signUp() async {}
+}

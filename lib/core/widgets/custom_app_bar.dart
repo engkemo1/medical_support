a52638a814e707ef3d11df994a60b4b3a78d2287
mohamed <mezoattalla123@gmail.com>
2024-04-28 20:36:46 +0300

    Updated CustomAppBar widget to include navigation to the cart view when the shopping cart icon is pressed.

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:medical_support/constants.dart';
+import 'package:medical_support/core/utils/app_router.dart';
 import 'package:medical_support/core/utils/styles.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key, required this.title});
-final String title ;
+
+  final String title;
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
@@ -13,26 +16,32 @@ final String title ;
       leading: IconButton(
         color: Colors.white,
         iconSize: 30.sp,
-        onPressed: (){},
-        icon: const Icon(Icons.menu ,
+        onPressed: () {},
+        icon: const Icon(
+          Icons.menu,
         ),
-
       ),
-      title: Text(title,
-      style: Styles.textStyle30ColorWeigh,),
+      title: Text(
+        title,
+        style: Styles.textStyle30ColorWeigh,
+      ),
       actions: [
         Padding(
           padding: const EdgeInsets.only(left: 8.0),
           child: IconButton(
             color: Colors.white,
             iconSize: 30.sp,
-            onPressed: (){},
+            onPressed: () {
+              Navigator.of(context).push(
+                AppRouter.router(
+                  const RouteSettings(name: AppRouter.kCartView),
+                ),
+              );
+            },
             icon: const Icon(Icons.shopping_cart),
-
           ),
         )
       ],
     );
   }
 }
-

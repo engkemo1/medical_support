30f4be7b2556bfdfa3fb18e20d76c18369ddb0ee
mohamed <mezoattalla123@gmail.com>
2024-04-29 14:41:53 +0300

    Refactor custom app bar to include navigation to drawer view and update app router to include drawer view functionality.

--- lib/features/drawer/presentation/views/widgets/drawer_view_body.dart
+++ lib/features/drawer/presentation/views/widgets/drawer_view_body.dart
@@ -1,12 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:medical_support/constants.dart';
+import 'package:medical_support/core/utils/app_router.dart';
+import 'package:medical_support/core/utils/styles.dart';
 
-class CartViewBody extends StatelessWidget {
-  const CartViewBody({
+class DrawerViewBody extends StatelessWidget {
+  const DrawerViewBody({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Drawer(
+        width: double.infinity,
+        backgroundColor: kPrimaryColor,
+        child: ListView(
+          children: [
+            const DrawerHeader(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                  size: 35,
+                ),
+                Divider(
+                  color: Colors.white,
+                  endIndent: 320,
+                  thickness: 4,
+                )
+              ],
+            )),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).push(
+                  AppRouter.router(
+                    const RouteSettings(
+                      name: AppRouter.kHomeView,
+                    ),
+                  ),
+                );
+              },
+              leading: const Icon(
+                size: 33,
+                Icons.dashboard_customize_outlined,
+                color: Colors.white,
+              ),
+              title: const Text("الاقسام", style: Styles.textStyle24ColorWhite),
+            ),
+            const Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Icon(
+                size: 33,
+                Icons.add_box_outlined,
+                color: Colors.white,
+              ),
+              title: const Text("المنتجات الجديدة",
+                  style: Styles.textStyle24ColorWhite),
+            ),
+            const Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Icon(
+                size: 33,
+                Icons.chat_outlined,
+                color: Colors.white,
+              ),
+              title: const Text("شكاوى ومقترحات",
+                  style: Styles.textStyle24ColorWhite),
+            ),
+            const Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Icon(
+                size: 33,
+                Icons.contact_phone_outlined,
+                color: Colors.white,
+              ),
+              title: const Text("تواصل معنا ",
+                  style: Styles.textStyle24ColorWhite),
+            ),
+            const Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Icon(
+                size: 33,
+                Icons.output,
+                color: Colors.white,
+              ),
+              title: const Text("تسجيل الخروج",
+                  style: Styles.textStyle24ColorWhite),
+            ),
+            const Divider(
+              thickness: 2,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

d08ea477313a97c49c6dfb79ecbca1b9aba6cf02
mohamed <mezoattalla123@gmail.com>
2024-04-29 13:58:04 +0300

    Added new files for drawer view body and drawer view in the project.Implemented basic structure for CartViewBody and CartView classes with necessary imports and build methods.

---
+++ lib/features/drawer/presentation/views/drawer_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/drawer_view_body.dart';
+
+
+class CartView extends StatelessWidget {
+  const CartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Directionality(
+      textDirection: TextDirection.rtl,
+      child: CartViewBody(),
+    );
+  }
+}

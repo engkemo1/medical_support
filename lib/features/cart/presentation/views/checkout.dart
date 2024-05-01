1ead007e189e6e448a9cc6f247b629a768c92ef7
mohamed <mezoattalla123@gmail.com>
2024-05-01 09:31:00 +0300

    Created new files for check out functionality in the cart feature. Added CheckOutBody widget to display the checkout screen and implemented it in the CheckOut widget for seamless integration.

---
+++ lib/features/cart/presentation/views/checkout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:medical_support/features/cart/presentation/views/widgets/check_out_bodu.dart';
+
+class CheckOut extends StatelessWidget {
+  const CheckOut({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  const CheckOutBody();
+  }
+}

56d7a8d268be95732bffb61cf7175a1fbbd471db
mohamed <mezoattalla123@gmail.com>
2024-05-02 13:39:55 +0300

    Added ContactUsView and ContactUsViewBody classes to display the contact us view in the app.

---
+++ lib/features/contactUs/presentation/views/contact_us_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'widgets/contact_us_view_body.dart';
+
+class ContactUsView extends StatelessWidget {
+  const ContactUsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Directionality(
+      textDirection: TextDirection.rtl,
+      child: ContactUsViewBody(),
+    );
+  }
+}

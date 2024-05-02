4a35e15d6a82968d430163edf4e71be68852f034
mohamed <mezoattalla123@gmail.com>
2024-05-02 13:58:50 +0300

    Updated contact us view body to include a custom app bar and contact information display. Made use of custom styles for text and icons.

--- lib/features/contactUs/presentation/views/widgets/contact_us_view_body.dart
+++ lib/features/contactUs/presentation/views/widgets/contact_us_view_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medical_support/core/utils/styles.dart';
+import 'package:medical_support/core/widgets/custom_app_bar.dart';
 
 class ContactUsViewBody extends StatelessWidget {
   const ContactUsViewBody({
@@ -7,8 +10,66 @@ class ContactUsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      body: Column(
+        children: [
+          const CustomAppBar(
+            title: "تواصل معنا ",
+            isBack: true,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10.h,
+                ),
+                const Text(
+                  "اذا اردت التواصل معنا من خلال ...:",
+                  style: Styles.textStyle24,
+                ),
+                SizedBox(
+                  height: 35.h,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: ListTile(
+                    onTap: () {},
+                    leading: const Icon(
+                      size: 25,
+                      Icons.phone,
+                    ),
+                    title:
+                        const Text("201101234567+", style: Styles.textStyle20),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.h,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: ListTile(
+                    onTap: () {},
+                    leading: const Icon(
+                      size: 25,
+                      Icons.email,
+                    ),
+                    title: const Text("medisup@gamil.com",
+                        style: Styles.textStyle20),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
